6fc1044be8de9cc3ee012e484347b5fc8baae410
eagleusb <git@eagleusb.com>
2023-02-21 16:29:18 +0100

    feat(testing) add redis cluster listing helper

--- magefile.go
+++ magefile.go
@@ -5,7 +5,6 @@ package main
 
 import (
 	"fmt"
-	"os/exec"
 
 	"github.com/magefile/mage/sh"
 )
@@ -15,16 +14,14 @@ var Aliases = map[string]interface{} {
   "s":     StopRedis,
 }
 
+var (
+	defaultRedisHost string = "0"
+	defaultRedisPort string = "32000"
+)
+
 // Default target to run when none is specified
 // If not set, running mage will list available targets
 var Default = StartRedis
-
-// Manage your deps, or running package managers.
-func InstallDeps() error {
-	fmt.Println("Installing Deps...")
-	cmd := exec.Command("go", "get", "github.com/stretchr/piglatin")
-	return cmd.Run()
-}
 
 func StartRedis() (err error) {
 	fmt.Print("Launching testing Redis cluster")
@@ -35,5 +32,11 @@ func StartRedis() (err error) {
 func StopRedis() (err error) {
 	fmt.Print("Stopping testing Redis cluster")
 	err = sh.RunV("docker-compose", "down", "--remove-orphans")
+	return
+}
+
+func ListRedis() (err error) {
+	fmt.Print("Listing testing Redis shards\n")
+	err = sh.RunV("redis-cli", "-h", defaultRedisHost, "-p", defaultRedisPort, "cluster", "nodes")
 	return
 }
